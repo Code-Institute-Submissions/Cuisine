349d02f89322e8ce5a2da460f5be03d357a8af79
Brook Kynaston <brookkynaston@live.fr>
2019-02-08 18:19:05 +0000

    Add recipe functionality added
    an app.py the abiliity to add a recipe has been developped (not done yet, needs to account for ingedients and instructions). Also addtask.html was added and displays options in select menus. Need to still add for instructions and ingredients

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
+
+
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipesdb'
 app.config["MONGO_URI"] = 'mongodb://admin-1:family_recipes1@ds125125.mlab.com:25125/recipesdb'
 
+
+
 mongo = PyMongo(app)
+
+
 
 @app.route('/')
 @app.route('/get_recipes')
@@ -21,6 +28,20 @@ def recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("recipe.html", recipe=the_recipe )
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html', 
+    serves=mongo.db.serves.find(), 
+    cooking_duration=mongo.db.cooking_duration.find(), 
+    cuisine_type=mongo.db.cuisine_type.find(),
+    meal_type=mongo.db.meal_type.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+    
 
 
 if __name__ == '__main__':
