ef556f77b2907703fd5d4e61eb6d955304f799b3
Brook Kynaston <brookkynaston@live.fr>
2019-02-11 17:35:44 +0000

    Continued styling recipe.html
    additional styling was added for recipe.html, including displaying all relevant recipe info. In, addition the parallax in base.html now displays different pictures based on what page the user is currently on

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ def get_recipes():
 def recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     
-    
     return render_template("recipe.html", recipe=the_recipe)
 
 @app.route('/add_recipe')
